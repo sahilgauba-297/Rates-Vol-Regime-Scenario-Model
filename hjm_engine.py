022f07d3941f2227ec6fda4096ff4af558cc7095
healthreflectcom <robin@healthreflect.com>
2026-05-04 22:55:07 -0400

    new updates

--- hjm_engine.py
+++ hjm_engine.py
@@ -26,28 +26,49 @@ drift_2Y = 0.01 * np.tanh(macro_signal)
 drift_10Y = 0.8 * drift_2Y
 
 
+# Simulating paths and steps
+n_paths = 1000
+n_steps = 100
 
-
-# monte carlo shocks
-shocks = np.random.multivariate_normal([0,0], cov_matrix, size = 100)
-
-#simulating future paths
 start_2Y = df["2Y"].iloc[-1]
 start_10Y = df["10Y"].iloc[-1]
 
-sim_2Y = [start_2Y]
-sim_10Y = [start_10Y]
+paths_2Y = np.zeros((n_paths, n_steps))
+paths_10Y = np.zeros((n_paths, n_steps))
 
-for i in range(100):
+for p in range(n_paths):
+    shocks = np.random.multivariate_normal([0,0], cov_matrix, size = n_steps)
 
-    shock_2Y, shock_10Y = shocks[i]
+    paths_2Y[p,0] = start_2Y
+    paths_10Y[p,0] = start_10Y
 
-    new_2Y = sim_2Y[-1]  + shock_2Y + drift_2Y
-    new_10Y = sim_10Y[-1] + shock_10Y + drift_10Y
+    for t in range(1, n_steps):
+        shock_2Y, shock_10Y = shocks[t]
 
-    sim_2Y.append(new_2Y)
-    sim_10Y.append(new_10Y)
+        paths_2Y[p, t] = paths_2Y[p, t-1] +shock_2Y + drift_2Y
+        paths_10Y[p,t] = paths_10Y[p, t-1] +shock_10Y + drift_10Y
 
 
-print("simulated 2Y path", sim_2Y[:5])
-print("simulated 10Y path", sim_10Y[:5])
+
+mean_2Y = paths_2Y.mean(axis = 0)
+mean_10Y = paths_10Y.mean(axis = 0)
+
+p5_2Y = np.percentile(paths_2Y, 5, axis = 0)
+p95_2Y = np.percentile(paths_2Y, 95, axis = 0)
+
+p5_10Y = np.percentile(paths_10Y, 5, axis = 0)
+p95_10Y = np.percentile(paths_10Y, 95, axis = 0)
+
+
+# steepening probability 
+
+initial_spread =  start_10Y  - start_2Y
+final_spread = paths_10Y[:, -1] - paths_2Y[:, -1]
+
+steepening_prob = np.mean(final_spread > initial_spread)
+
+
+print("Steepening probability:", round(steepening_prob, 4))
+
+print("Mean final 2Y:", mean_2Y[-1])
+print("Mean final 10Y:", mean_10Y[-1])
